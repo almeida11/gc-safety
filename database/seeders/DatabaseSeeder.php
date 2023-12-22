2bb2f37e14018b1d788ccbb462a8ca10319441cd
Gabriel Gigante <q1dcc21@gmail.com>
2023-12-22 20:34:06 +0000

    chore: Rename seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,65 +12,44 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        /* \App\Models\User::factory(10)->create();
-
-         \App\Models\User::factory()->create([
-             'name' => 'Test User',
-             'email' => 'test@example.com',
-          ]);
-          
-        'name',
-        'email',
-        'password',
-        'type',
-        'company',
-        'active',
-          */
 
         \DB::table('companies')->insert([
             [
-                'razao_social' => 'Gigante Sistemas S/A',
-                'name' => 'Gigante Sistemas',
+                'razao_social' => 'GC Sistemas S/A',
+                'name' => 'GC Sistemas',
                 'tipo' => "Contratante",
             ],
             [
-                'razao_social' => 'Mateus Sistemas S/A',
-                'name' => 'Mateus Sistemas',
+                'razao_social' => 'GC Safety S/A',
+                'name' => 'GC Safety',
                 'tipo' => 'Contratada',
             ]
         ]);
         \DB::table('users')->insert([
             [
                 'name' => 'Pedro Gabriel Gigante Roseno Lima',
-                'email' => 'gabriel.gigante@gigantesistemas.com.br',
+                'email' => 'gabriel.gigante@gcsistemas.com.br',
                 'password' => \Hash::make('12345678'),
                 'type' => 'Administrador',
                 'active' => true,
             ],
             [
                 'name' => 'Mateus de Almeida',
-                'email' => 'mateus.almeida@gigantesistemas.com.br',
+                'email' => 'mateus.almeida@gcsistemas.com.br',
                 'password' => \Hash::make('12345678'),
                 'type' => 'Administrador',
                 'active' => true,
             ],
             [
                 'name' => 'Pablo',
-                'email' => 'pablo@gigantesistemas.com.br',
+                'email' => 'pablo@gcsistemas.com.br',
                 'password' => \Hash::make('12345678'),
                 'type' => 'Moderador',
                 'active' => true,
             ],
             [
                 'name' => 'Vinicius',
-                'email' => 'vinicius@gigantesistemas.com.br',
-                'password' => \Hash::make('12345678'),
-                'type' => 'Moderador',
-                'active' => true,
-            ],
-            [
-                'name' => 'Teste',
-                'email' => 'Teste@gigantesistemas.com.br',
+                'email' => 'vinicius@gcsistemas.com.br',
                 'password' => \Hash::make('12345678'),
                 'type' => 'Moderador',
                 'active' => true,
@@ -96,21 +75,14 @@ class DatabaseSeeder extends Seeder
                 'id_company' => 1,
                 'id_user' => 4,
                 'is_manager' => 0,
-            ],
-            [
-                'id_company' => 1,
-                'id_user' => 5,
-                'is_manager' => 0,
             ]
         ]);
+        
         \DB::table('company_relations')->insert([
             [
                 'id_contratante' => 1,
                 'id_contratada' => 2,
             ]
         ]);
-        // User::factory()->count(50)->create();
-        // User_relation::factory()->count(50)->create();
-        // Employee::factory()->count(50)->create();
     }
 }
